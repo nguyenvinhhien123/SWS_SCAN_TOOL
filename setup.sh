075cd529349a66709d1770f07843f1aaea93812b
p0wershe1l <44284059+nguyenvinhhien123@users.noreply.github.com>
2026-01-18 19:30:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-
+echo "[+] Dang cap nhat he thong..."
 sudo apt update
 
+echo "[+] Dang sua loi dependencies va cai dat cong cu..."
+# Cai dat tung buoc de tranh loi docker
+sudo apt install -y containerd
+sudo apt install -y docker.io nmap nikto wapiti golang-go
 
-sudo apt install -y nmap nikto wapiti docker.io golang-go
-
+echo "[+] Dang cai dat Katana..."
+# Su dung duong dan day du cua Go
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/lib/go/bin:$GOPATH/bin
 
 go install github.com/projectdiscovery/katana/cmd/katana@latest
-sudo cp ~/go/bin/katana /usr/local/bin/
 
+# Kiem tra va copy file thuc thi
+if [ -f "$HOME/go/bin/katana" ]; then
+    sudo cp $HOME/go/bin/katana /usr/local/bin/
+    echo "[+] Da cai dat Katana thanh cong."
+else
+    echo "[!] Khong tim thay file katana sau khi build."
+fi
 
 chmod +x pentestsws.py
-
-echo "done."
+echo "--- XONG ---"
